c217dbd3faeb9e1e76c02a8a13990fd969d078b9
Galahad04 <shakirov_a@auca.kg>
2022-08-01 15:59:30 +0600

    heroku deployment

--- mb/settings.py
+++ mb/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-gv$vvt+e!$y(qmq9g1vjhwigu+59g)@t)_z&n%xr)1==rv*++o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
